864f465457bdd847f094badc29da8803a7166a93
Washingtoncip <washingtoncip@outlook.com>
2026-03-14 14:10:10 -0300

    Exercício 4

---
+++ Atividade04/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atividade04</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+            <pre>
+                <label for="">Digite a temperatura em Celsius!</label>
+                <input type="text" name="celsius" id="">
+                <input type="submit" value="Enviar">
+                <input type="reset" value="Limpar">
+            </pre>
+
+            <?php
+     
+                $c = $_POST["celsius"];
+                $f = $c *9/5 + 32;
+
+                echo "<br>";
+                echo "A temperatura digitada em C° " .$c;
+                echo "<br>";
+                echo "Em Fahrenheit são = " .$f. " ° F";
+            ?>
+    </form>
+</body>
+</html>
+
