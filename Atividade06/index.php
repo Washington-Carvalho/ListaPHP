213544757291a4c5de6bd2aa03692c9c75b48204
Washingtoncip <washingtoncip@outlook.com>
2026-03-14 14:10:55 -0300

    Exercício 6

---
+++ Atividade06/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atividade06</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+            <pre>
+                <label for="">Digite um valor R$:</label>
+                <input type="text" name="dolar" id="">
+                <input type="submit" value="Enviar">
+                <input type="reset" value="Limpar">
+            </pre>
+
+            <?php
+     
+                $n1 = $_POST["dolar"];
+                $r = $n1 * 5.17; 
+
+                echo "<br>";
+                echo "O Valor de USD $ " .$n1. "<br> Em Reais é R$" .$r;
+            ?>
+    </form>
+</body>
+</html>
+
